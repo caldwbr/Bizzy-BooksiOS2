9c20c08afa9082898b3aeae93ab6d8811cb0e516
Caldwell Contracting <caldwbr@gmail.com>
2023-11-22 11:14:05 -0600

    first commit

---
+++ Bizzy-Books/Bizzy_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bizzy_BooksApp.swift
+//  Bizzy-Books
+//
+//  Created by Brad Caldwell on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Bizzy_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
